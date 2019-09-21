3a91a9f5303f59d42fef07122bbeec94958c2208
Zira Cook <ziracook@gmail.com>
2019-09-20 23:40:24 -0700

    Blocks are now moveable in specific ways

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+    public float xPos;
+    public float yPos;
+    public float zPos;
+
+    public void OnMouseDrag()
+    {
+        transform.position = Input.mousePosition;
+
+    }
+
+    // Y stays the same, X changes
+    public void OnDragHorizontal()
+    {
+        xPos = Input.mousePosition.x;
+        yPos = transform.position.y;
+        transform.position = new Vector3(xPos, yPos);
+    }
+
+    // X stays the same, Y changes
+    public void OnDragVertical()
+    {
+        xPos = transform.position.x;
+        yPos = Input.mousePosition.y;
+        transform.position = new Vector3(xPos, yPos);
+    }
+}
